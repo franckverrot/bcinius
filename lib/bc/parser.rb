d0b6d7fb922411c57b5923abf1eafad11f8309e4
Franck Verrot <franck@verrot.fr>
2014-03-12 11:01:21 +0100

    Refactor float parsing

--- lib/bc/parser.rb
+++ lib/bc/parser.rb
@@ -1,24 +1,25 @@
 module BC
   class Parser
     def parse(str)
-      pos = 0
-      unit = decimal = ""
-      while pos < str.length && /\d/.match(str[pos])
-        unit += str[pos]
-        pos += 1
-      end
+      decimal = ""
+      unit, pos = consume_number_literal(str, 0)
       if /\./.match(str[pos])
-        pos += 1
-        while pos < str.length && /\d/.match(str[pos])
-          decimal += str[pos]
-          pos += 1
-        end
+        decimal, pos = consume_number_literal(str, pos + 1)
       end
       if decimal.empty?
         Integer(unit)
       else
         Float("#{unit}.#{decimal}")
       end
+    end
+
+    def consume_number_literal(str, pos)
+      token = ""
+      while pos < str.length && /\d/.match(str[pos])
+        token += str[pos]
+        pos += 1
+      end
+      return [token, pos]
     end
   end
 end
