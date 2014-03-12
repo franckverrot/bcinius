9bd500ee22914e01fd3d0d0664829c718ac6cf76
Franck Verrot <franck@verrot.fr>
2014-03-12 10:58:03 +0100

    Implement float support

--- lib/bc/parser.rb
+++ lib/bc/parser.rb
@@ -1,7 +1,24 @@
 module BC
   class Parser
     def parse(str)
-      str.to_i
+      pos = 0
+      unit = decimal = ""
+      while pos < str.length && /\d/.match(str[pos])
+        unit += str[pos]
+        pos += 1
+      end
+      if /\./.match(str[pos])
+        pos += 1
+        while pos < str.length && /\d/.match(str[pos])
+          decimal += str[pos]
+          pos += 1
+        end
+      end
+      if decimal.empty?
+        Integer(unit)
+      else
+        Float("#{unit}.#{decimal}")
+      end
     end
   end
 end
--- test/bc_test.rb
+++ test/bc_test.rb
@@ -8,5 +8,10 @@ describe "BC" do
     it "should recognize a number literal" do
       subject.parse("42").must_equal 42
     end
+
+    it "should recognize a float literal" do
+      subject.parse("4.2").must_equal 4.2
+    end
+
   end
 end
